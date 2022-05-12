e5019a4babeb6e6816b75367d18197f446604acb
ninew089 <jittanan.jck@gmail.com>
2022-05-12 21:45:10 +0700

    remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
 // routes
-import Router from './routes';
+import Router from "./routes";
 // theme
-import ThemeProvider from './theme';
+import ThemeProvider from "./theme";
 // components
-import Settings from './components/settings';
-import RtlLayout from './components/RtlLayout';
-import { ChartStyle } from './components/chart';
-import ScrollToTop from './components/ScrollToTop';
-import { ProgressBarStyle } from './components/ProgressBar';
-import NotistackProvider from './components/NotistackProvider';
-import ThemeColorPresets from './components/ThemeColorPresets';
-import ThemeLocalization from './components/ThemeLocalization';
-import MotionLazyContainer from './components/animate/MotionLazyContainer';
+import ScrollToTop from "./components/ScrollToTop";
+import { ProgressBarStyle } from "./components/ProgressBar";
+import NotistackProvider from "./components/NotistackProvider";
+
+import MotionLazyContainer from "./components/animate/MotionLazyContainer";
 
 // ----------------------------------------------------------------------
 
 export default function App() {
   return (
     <ThemeProvider>
-      <ThemeColorPresets>
-        <ThemeLocalization>
-          <RtlLayout>
-            <NotistackProvider>
-              <MotionLazyContainer>
-                <ProgressBarStyle />
-                <ChartStyle />
-                <Settings />
-                <ScrollToTop />
-                <Router />
-              </MotionLazyContainer>
-            </NotistackProvider>
-          </RtlLayout>
-        </ThemeLocalization>
-      </ThemeColorPresets>
+      <NotistackProvider>
+        <MotionLazyContainer>
+          <ProgressBarStyle />
+          <ScrollToTop />
+          <Router />
+        </MotionLazyContainer>
+      </NotistackProvider>
     </ThemeProvider>
   );
 }
